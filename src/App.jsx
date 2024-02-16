9b72e60b9de9145dc9a99f21239edf83353745ae
Divya <divyanamdev8795@gmail.com>
2024-02-16 20:38:16 +0530

    feat:add stepper

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import "./App.css";
-
+import Stepper from "./components/Stepper";
+ const CHECKOUT_STEP =[
+{
+  name:"Customer Info",
+  component:()=> <div>Provide your Contact Details</div>
+},
+{
+  name:"Shipping Info",
+  component:()=><div>Enter your shipping details here</div>
+},
+{
+  name:"Payment",
+  component:()=><div>Complete your payment for your order </div>
+},
+{
+  name:"Delivered",
+  component:()=><div>Your order has been delivered</div>
+}
+]
 function App() {
   return (
     <>
-      <div>Hello Coder!</div>
+      <div>
+        <h2>Checkout</h2>
+        <Stepper stepsConfig={CHECKOUT_STEP}/>
+      </div>
     </>
   );
 }
